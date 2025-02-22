0d1ed5201d68c5a954454b35f90db19b6fd2985f
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-21 18:11:59 -0800

    Get pkg info

--- nix_playground/checkout.py
+++ nix_playground/checkout.py
@@ -1,4 +1,7 @@
+import json
 import logging
+import subprocess
+import sys
 
 import click
 
@@ -10,7 +13,23 @@ logger = logging.getLogger(__name__)
 
 
 @cli.command(name="checkout", help="Checkout nixpkgs source content locally")
-@click.argument("NAME", type=str)
+@click.argument("PKG_NAME", type=str)
 @pass_env
-def main(env: Environment, name: str):
-    logger.info("Checkout out nixpkgs %s")
+def main(env: Environment, pkg_name: str):
+    logger.info("Checkout out nixpkgs %s ...", pkg_name)
+    try:
+        pkg_path = json.loads(
+            subprocess.check_output(["nix", "eval", "--json", pkg_name])
+        )
+    except subprocess.CalledProcessError:
+        logger.error("Failed to eval package %s", pkg_name)
+        sys.exit(-1)
+    logger.info("Got package path %s", pkg_path)
+    pkg_der_path = (
+        subprocess.check_output(["nix-store", "--query", "--deriver", pkg_path])
+        .decode("utf8")
+        .strip()
+    )
+    logger.info("Got package der path %s", pkg_der_path)
+    der_payload = subprocess.check_output(["nix", "derivation", "show", pkg_der_path])
+    logger.info("Got package der path %s", der_payload)
