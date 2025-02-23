11df6090127b7d12810aea657aef0aa3acbd47fb
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-23 11:56:56 -0800

    More test cases, fix broken tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -11,6 +11,10 @@ from nix_playground.utils import switch_cwd
     "pkg_name, expected_result_files",
     [
         ("nixpkgs#cowsay", [pathlib.Path("bin") / "cowsay"]),
+        (
+            "github:NixOS/nixpkgs/a3a3dda3bacf61e8a39258a0ed9c924eeca8e293#cowsay",
+            [pathlib.Path("bin") / "cowsay"],
+        ),
         ("nixpkgs#hello", [pathlib.Path("bin") / "hello"]),
         ("nixpkgs#libnvidia-container", [pathlib.Path("bin") / "nvidia-container-cli"]),
     ],
