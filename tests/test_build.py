42e2baa0207927250a5146095704f28a23ecc6eb
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-23 11:38:06 -0800

    Fix build tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -11,9 +11,7 @@ from nix_playground.utils import switch_cwd
     "pkg_name, expected_result_files",
     [
         ("nixpkgs#cowsay", [pathlib.Path("bin") / "cowsay"]),
-        ("cowsay", [pathlib.Path("bin") / "cowsay"]),
         ("nixpkgs#libnvidia-container", [pathlib.Path("bin") / "nvidia-container-cli"]),
-        ("libnvidia-container", [pathlib.Path("bin") / "nvidia-container-cli"]),
     ],
 )
 def test_build(
