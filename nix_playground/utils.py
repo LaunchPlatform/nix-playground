108246cee59ae43965fc59682d7d52cd57b6fc49
Fang-Pen Lin <hello@fangpenlin.com>
2025-03-09 13:30:55 -0700

    Add apply patch func

--- nix_playground/utils.py
+++ nix_playground/utils.py
@@ -4,9 +4,12 @@ import io
 import logging
 import os
 import pathlib
+import subprocess
 import sys
 import tarfile
 import typing
+
+import pygit2
 
 from . import constants
 
@@ -62,3 +65,35 @@ def extract_tar(
         else:
             logger.warning("Performing unsafe tar file extracting")
             tar_file.extractall(**extra_kwargs)
+
+
+def apply_patch(
+    repo: pygit2.Repository,
+    patch_file: pathlib.Path,
+    patch_bin: pathlib.Path | None = None,
+):
+    # TODO: ideally, we should support patch file in different formats, like what nixpkg's setup.sh provides.
+    #       https://github.com/NixOS/nixpkgs/blob/09e25be882ef7751b2e4bbc3aa4e2df1c4613558/pkgs/stdenv/generic/setup.sh#L1386C1-L1400C13
+    #       however, not sure how common these type of patches files are. Let's ignore it for now, and let the user
+    #       report the issues and implement it if that's really needed
+    if patch_file.suffix in {".gz", ".bz2", ".xz", ".lzma"}:
+        logger.error(
+            "Currently compressed patch file is not supported yet, please see the issue https://github.com/LaunchPlatform/nix-playground/issues/FIXME to learn more"
+        )
+        raise
+
+    diff = pygit2.Diff.parse_diff(patch_file.read_bytes())
+    try:
+        repo.apply(diff)
+    except pygit2.GitError as exc:
+        logger.info(
+            "Failed to patch with git, fall back to use patch command instead. Error: %s",
+            exc,
+        )
+        if patch_bin is None:
+            logger.error(
+                "Need to provide patch command line executable via --patch-bin or have it in the PATH"
+            )
+            sys.exit(-1)
+        with switch_cwd(repo.path):
+            subprocess.check_call([patch_bin, "-i", str(patch_file), "-p1"])
